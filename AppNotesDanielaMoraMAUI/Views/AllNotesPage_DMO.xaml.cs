1966b741557790d26d57b1784a547441fac39906
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-16 14:04:19 -0500

    Cambios de la estructura del archivo All Notes Page de la carpeta View

--- AppNotesDanielaMoraMAUI/Views/AllNotesPage_DMO.xaml.cs
+++ AppNotesDanielaMoraMAUI/Views/AllNotesPage_DMO.xaml.cs
@@ -5,32 +5,11 @@ namespace AppNotesDanielaMoraMAUI.Views
         public AllNotesPage_DMO()
         {
             InitializeComponent();
-            BindingContext = new Models.AllNotes_DMO();
         }
 
-        protected override void OnAppearing()
+        private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
         {
-            ((Models.AllNotes_DMO)BindingContext).LoadNotes();
-        }
-
-        private async void Add_Clicked_DMO(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync(nameof(NotePageDMO));
-        }
-
-        private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.CurrentSelection.Count != 0)
-            {
-                // Get the note model
-                var note = (Models.Note_DMO)e.CurrentSelection[0];
-
-                // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
-                await Shell.Current.GoToAsync($"{nameof(NotePageDMO)}?{nameof(NotePageDMO.ItemId)}={note.Filename_DMO}");
-
-                // Unselect the UI
-                notesCollection.SelectedItem = null;
-            }
+            notesCollection.SelectedItem = null;
         }
     }
 }
